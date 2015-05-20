43db487b32cb56f2e148f14731d8cb1b03a038a3
William Shaw <williamfshaw@icloud.com>
2015-05-20 13:15:56 -0400

    fixed bg

--- js/page.js
+++ js/page.js
@@ -9,7 +9,7 @@ function renderAbout() {
   var template = Handlebars.compile($('#about-me-temp').html())
   var data = {
     name: 'William Shaw',
-    about: ['  Hi! My name is Will. I am a Junior Fullstack Web Developer located in New York City. I am currently an instructor of the Web Development Immersive program at General Assembly teaching incoming student to program in Javascript and Ruby. Before this I was in sales, as much as I enjoyed sales, I found it did not fulfill the desire to I have to be creative nor did it push my technical abilities.', ' In my free time I am an avid sports fan, Lets go Mets!! I play baseball, softball and I belong to a crossfit gym (No worries, I haven\'t drank the crossfit kool-aid).'].join('\n')
+    about: ['  Hi! My name is Will. I am a Fullstack Web Developer located in New York City. Currently I am an instructor of the Web Development Immersive program at General Assembly teaching incoming students to program in Javascript and Ruby. Before teaching this course I was a student in WDI, prior to that I was in sales, as much as I enjoyed sales, I found it did not fulfill the desire to I have to be creative nor did it push my technical abilities.', ' In my free time I am an avid sports fan, Lets go Mets!! I play baseball, softball and I belong to a crossfit gym (No worries, I haven\'t drank the crossfit kool-aid).'].join('\n')
   }
   $('.info-container').append(template(data));
 };
